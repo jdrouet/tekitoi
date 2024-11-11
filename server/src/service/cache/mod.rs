1a7710ace47ae8db1aa52c439bf66c959d34af1a
Jérémie Drouet <jeremie.drouet@gmail.com>
2024-11-11 17:18:12 +0100

    feat: allow cache client to be something else
    Signed-off-by: Jérémie Drouet <jeremie.drouet@gmail.com>

--- server/src/service/cache/mod.rs
+++ server/src/service/cache/mod.rs
@@ -1,67 +1,46 @@
-use std::time::Duration;
+mod memory;
 
 #[derive(Debug)]
 pub(crate) enum Config {
-    Memory {
-        max_capacity: u64,
-        // A cached entry will be expired after the specified duration past from insert.
-        time_to_live: u64,
-    },
+    Memory(memory::Config),
 }
 
 impl Config {
     pub(crate) fn from_env() -> anyhow::Result<Self> {
-        let max_capacity = crate::helper::parse_env_or("CACHE_MAX_CAPACITY", 100)?;
-        let time_to_live = crate::helper::parse_env_or("CACHE_TIME_TO_LIVE", 60 * 10)?;
-
-        Ok(Self::Memory {
-            max_capacity,
-            time_to_live,
-        })
+        match crate::helper::from_env_or("CACHE_KIND", "memory").as_ref() {
+            "memory" => memory::Config::from_env().map(Config::Memory),
+            other => Err(anyhow::anyhow!("unknown cache kind {other:?}")),
+        }
     }
 
     pub(crate) fn build(self) -> anyhow::Result<Client> {
         match self {
-            Self::Memory {
-                max_capacity,
-                time_to_live,
-            } => {
-                let inner = moka::future::CacheBuilder::default()
-                    .max_capacity(max_capacity)
-                    .time_to_live(Duration::from_secs(time_to_live))
-                    .build();
-                Ok(Client { inner })
-            }
+            Self::Memory(inner) => inner.build().map(Client::Memory),
         }
     }
 }
 
 #[derive(Clone, Debug)]
-pub(crate) struct Client {
-    inner: moka::future::Cache<String, String>,
+pub(crate) enum Client {
+    Memory(memory::Client),
 }
 
 impl Client {
     pub async fn remove<V: serde::de::DeserializeOwned>(&self, key: &str) -> Option<V> {
-        self.inner
-            .remove(key)
-            .await
-            .and_then(|v| serde_urlencoded::from_str(v.as_str()).ok())
+        match self {
+            Self::Memory(inner) => inner.remove(key).await,
+        }
     }
 
     pub async fn get<V: serde::de::DeserializeOwned>(&self, key: &str) -> Option<V> {
-        self.inner
-            .get(key)
-            .await
-            .and_then(|v| serde_urlencoded::from_str(v.as_str()).ok())
+        match self {
+            Self::Memory(inner) => inner.get(key).await,
+        }
     }
 
     pub async fn insert<V: serde::Serialize>(&self, key: String, value: &V) {
-        match serde_urlencoded::to_string(value) {
-            Ok(encoded) => self.inner.insert(key, encoded).await,
-            Err(err) => {
-                tracing::error!(message = "unable to insert in cache", cause = %err);
-            }
+        match self {
+            Self::Memory(inner) => inner.insert(key, value).await,
         }
     }
 }
@@ -69,10 +48,6 @@ impl Client {
 #[cfg(test)]
 impl Client {
     pub(crate) fn test() -> Self {
-        let inner = moka::future::CacheBuilder::default()
-            .max_capacity(100)
-            .time_to_live(Duration::from_secs(10))
-            .build();
-        Client { inner }
+        Client::Memory(memory::Client::test())
     }
 }
